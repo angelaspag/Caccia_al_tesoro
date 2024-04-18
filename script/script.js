cf6802b649e82c95718e9ca0ec4bb66d7a448656
angelaspag <162449057+angelaspag@users.noreply.github.com>
2024-04-18 11:22:56 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 function doLogin() {
- document.getElementById()
+ var username = document.getElementById("username").value;
+ var password = document.getElementById("password").value;
+ if (username == "" || password == "") { 
+   alert (" Username e password sono obbligatori ");
+ }
 }
